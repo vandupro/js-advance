fd872f69d05d644d46d37e5915637e7bb42de159
unknown <dunvph10007@fpt.edu.vn>
2021-01-26 10:08:32 +0700

    26/1

--- frontend/src/pages/HomPage.js
+++ frontend/src/pages/HomPage.js
@@ -1,11 +1,21 @@
 import data from '../data.js';
 export default class HomPage{
 	render(){
-		//const { products } = data;
-		//console.log(data);
-		
-		return `
-				
-		`
+		const { products } = data;
+		//console.log(products);
+		const $ = document.querySelector.bind('document');
+		const result = products.map((product,index)=>{
+            return`<tr>
+            <td>${index +1}</td>
+            <td>${product.name}</td>
+            <td>${product.price}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-detail" data-id="${product.id}">Chi tiết</button>
+                <button type="button" class="btn btn-danger btn-remove" data-id="${product.id}">Chi tiết</button>
+            </td>
+            </tr>`
+        }).join("");
+       return result;
+
 	}
 }
